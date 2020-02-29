96f9b7b1df1f5b915c6286b5f714dab8a9835b28
Alex Stephens <astephens7000@yahoo.com>
2020-02-29 13:42:24 -0500

    Operational code, tweak superparameters

--- CNN_flower_classification.py
+++ CNN_flower_classification.py
@@ -5,10 +5,12 @@ import os
 import shutil
 from pathlib import Path
 
-import cv2
+### plot the training history
 import matplotlib.pyplot as plt
 import numpy as np
 from keras import layers, models, utils
+
+import cv2
 
 ###############################################################################
 
@@ -46,7 +48,39 @@ TEST_ROSE_PATH  = os.path.join(TEST_PATH, "rose")
 TEST_SUN_PATH   = os.path.join(TEST_PATH, "sunflower")
 TEST_TULIP_PATH = os.path.join(TEST_PATH, "tulip")
 
-INPUT_IMG_SIZE = (28, 28)
+LABELS = [
+    [1, 0, 0, 0, 0], # Daisy
+    [0, 1, 0, 0, 0], # Dandelion
+    [0, 0, 1, 0, 0], # Rose
+    [0, 0, 0, 1, 0], # Sun
+    [0, 0, 0, 0, 1], # Tulip
+]
+
+TRAIN_PATHS = [
+    TRAIN_DAISY_PATH,
+    TRAIN_DANDE_PATH,
+    TRAIN_ROSE_PATH,
+    TRAIN_SUN_PATH,
+    TRAIN_TULIP_PATH
+]
+
+VAL_PATHS = [
+    VAL_DAISY_PATH,
+    VAL_DANDE_PATH,
+    VAL_ROSE_PATH,
+    VAL_SUN_PATH,
+    VAL_TULIP_PATH
+]
+
+TEST_PATHS = [
+    TEST_DAISY_PATH,
+    TEST_DANDE_PATH,
+    TEST_ROSE_PATH,
+    TEST_SUN_PATH,
+    TEST_TULIP_PATH
+]
+
+INPUT_IMG_SIZE = (100, 100)
 
 ###############################################################################
 
@@ -68,20 +102,23 @@ def partition_sets(
     for i, fname in enumerate(fnames_dst):
         src = os.path.join(src_path, fnames_src[i])
         dst = os.path.join(val_path, fname)
-        shutil.copyfile(src, dst)
+        if src != dst:
+            shutil.copyfile(src, dst)
 
     fnames_dst = ["{}.{}.jpg".format(class_name, i) for i in range(100, 200)]
     for i, fname in enumerate(fnames_dst):
         src = os.path.join(src_path, fnames_src[i + 100])
         dst = os.path.join(test_path, fname)
-        shutil.copyfile(src, dst)
+        if src != dst:
+            shutil.copyfile(src, dst)
 
     size = len(fnames_src)
     fnames_dst = ["{}.{}.jpg".format(class_name, i) for i in range(200, size)]
     for i, fname in enumerate(fnames_dst):
         src = os.path.join(src_path, fnames_src[i + 200])
         dst = os.path.join(training_path, fname)
-        shutil.copyfile(src, dst)
+        if src != dst:
+            shutil.copyfile(src, dst)
 
 def resize_images(src_path, new_size):
     if not isinstance(src_path, str):
@@ -92,15 +129,31 @@ def resize_images(src_path, new_size):
     else:
         raise Exception("Given size must be a tuple.")
 
-    os.chdir(src_path)
-    for fname in os.listdir(src_path):
-        if fname.endswith(".jpg"):
-            file = os.path.join(src_path, fname)
-            src_img = cv2.imread(file)
+    fnames = glob.glob("{}/*.jpg".format(src_path))
+    for fname in fnames:
+        file = os.path.join(src_path, fname)
+        src_img = cv2.imread(file)
+        if src_img.shape != new_size:
             new_img = np.copy(src_img)
             new_img = cv2.resize(new_img, new_size)
+            os.chdir(src_path)
             cv2.imwrite(fname, new_img)
-    os.chdir(BASE_PATH)
+            os.chdir(BASE_PATH)
+
+def add_imgs_labels(src_path, src_imgs, src_labels, label):
+    if not isinstance(src_path, str):
+        raise Exception("src_path must be a list.")
+    if not isinstance(src_imgs, list):
+        raise Exception("src_imgs must be a list.")
+    if not isinstance(src_labels, list):
+        raise Exception("src_labels must be a list.")
+
+    fnames = glob.glob("{}/*.jpg".format(src_path))
+    for fname in fnames:
+        img = cv2.imread(fname)
+        src_imgs.append(img)
+        src_labels.append(label)
+    return src_imgs, src_labels
 
 ###############################################################################
 
@@ -136,6 +189,7 @@ Path(TEST_TULIP_PATH).mkdir(parents=True, exist_ok=True)
 
 ###############################################################################
 
+print("Partitioning dataset, this could take a while...")
 partition_sets(DATA_DAISY_PATH, VAL_DAISY_PATH, 
                TEST_DAISY_PATH, TRAIN_DAISY_PATH, "daisy")
 partition_sets(DATA_DANDE_PATH, VAL_DANDE_PATH,
@@ -146,9 +200,11 @@ partition_sets(DATA_SUN_PATH, VAL_SUN_PATH,
                TEST_SUN_PATH, TRAIN_SUN_PATH, "sunflower")
 partition_sets(DATA_TULIP_PATH, VAL_TULIP_PATH,
                TEST_TULIP_PATH, TRAIN_TULIP_PATH, "tulip")
+print("Partitioning completed.")
 
 ###############################################################################
 
+print("Resizing images, this could take a while...")
 resize_images(VAL_DAISY_PATH, INPUT_IMG_SIZE)
 resize_images(VAL_DANDE_PATH, INPUT_IMG_SIZE)
 resize_images(VAL_ROSE_PATH, INPUT_IMG_SIZE)
@@ -164,6 +220,91 @@ resize_images(TRAIN_DANDE_PATH, INPUT_IMG_SIZE)
 resize_images(TRAIN_ROSE_PATH, INPUT_IMG_SIZE)
 resize_images(TRAIN_SUN_PATH, INPUT_IMG_SIZE)
 resize_images(TRAIN_TULIP_PATH, INPUT_IMG_SIZE)
+print("Resizing completed.")
 
 ###############################################################################
 
+# prepare training image set and labels
+train_images = []
+train_labels = []
+
+for i, path in enumerate(TRAIN_PATHS):
+    train_images, train_labels = add_imgs_labels(path, train_images,
+                                                 train_labels, LABELS[i])
+
+train_images = np.array(train_images)
+train_labels = np.array(train_labels)
+
+# prepare validation image set and labels
+val_images = []
+val_labels = []
+
+for i, path in enumerate(VAL_PATHS):
+    val_images, val_labels = add_imgs_labels(path, val_images,
+                                             val_labels, LABELS[i])
+
+val_images = np.array(val_images)
+val_labels = np.array(val_labels)
+
+# prepare test image set and labels
+test_images = []
+test_labels = []
+
+for i, path in enumerate(TEST_PATHS):
+    test_images, test_labels = add_imgs_labels(path, test_images,
+                                               test_labels, LABELS[i])
+
+test_images = np.array(test_images)
+test_labels = np.array(test_labels)
+
+# create network architecture
+network = models.Sequential()
+
+network.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=test_images[0].shape))
+network.add(layers.MaxPooling2D((2, 2)))
+network.add(layers.Conv2D(64, (3, 3), activation='relu'))
+network.add(layers.MaxPooling2D((2, 2)))
+network.add(layers.Conv2D(64, (3, 3), activation='relu'))
+network.add(layers.Flatten())
+network.add(layers.Dense(64, activation='relu'))
+network.add(layers.Dense(5, activation='softmax'))
+
+network.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+# normalize inputs
+train_images = train_images.astype('float32') / 255
+test_images = test_images.astype('float32') / 255
+
+# fit network
+history=network.fit(train_images, train_labels, epochs=15, batch_size=50,
+                    validation_data=(val_images, val_labels))
+
+# plotting network statistics
+acc = history.history['accuracy']
+loss = history.history['loss']
+val_acc = history.history['val_accuracy']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.figure(1)
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'bo', label='Validation accuracy')
+plt.title('Training accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.figure(2)
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'bo', label='Validation accuracy')
+plt.title('Training loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# test model
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print(test_acc)
