59fbc0b10ca15e1e0792f5a979fb9b3464fc0ba1
Alex Stephens <astephens7000@yahoo.com>
2020-03-06 14:58:31 -0500

    changed to transfer learning with VGG

--- CNN_flower_classification.py
+++ CNN_flower_classification.py
@@ -12,10 +12,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 # from keras import backend as K # uncomment to run on Alex's PC for AMD GPU
-from keras import layers, models, utils
+from keras import layers, models, applications, optimizers
 from keras.preprocessing import image
-
-import cv2
 
 ###############################################################################
 
@@ -28,6 +26,22 @@ DATA_DANDE_PATH = os.path.join(DATA_PATH, "dandelion")
 DATA_ROSE_PATH  = os.path.join(DATA_PATH, "roses")
 DATA_SUN_PATH   = os.path.join(DATA_PATH, "sunflowers")
 DATA_TULIP_PATH = os.path.join(DATA_PATH, "tulips")
+
+DATA_PATHS = [
+    DATA_DAISY_PATH,
+    DATA_DANDE_PATH,
+    DATA_ROSE_PATH,
+    DATA_SUN_PATH,
+    DATA_TULIP_PATH
+]
+
+CLASSES = [
+    "daisy",
+    "dandelion",
+    "rose",
+    "sunflower",
+    "tulip"
+]
 
 # paths for training set
 TRAIN_PATH       = os.path.join(BASE_PATH, "training_set")
@@ -53,14 +67,6 @@ TEST_ROSE_PATH  = os.path.join(TEST_PATH, "rose")
 TEST_SUN_PATH   = os.path.join(TEST_PATH, "sunflower")
 TEST_TULIP_PATH = os.path.join(TEST_PATH, "tulip")
 
-LABELS = [
-    [1, 0, 0, 0, 0], # Daisy
-    [0, 1, 0, 0, 0], # Dandelion
-    [0, 0, 1, 0, 0], # Rose
-    [0, 0, 0, 1, 0], # Sun
-    [0, 0, 0, 0, 1], # Tulip
-]
-
 TRAIN_PATHS = [
     TRAIN_DAISY_PATH,
     TRAIN_DANDE_PATH,
@@ -85,7 +91,15 @@ TEST_PATHS = [
     TEST_TULIP_PATH
 ]
 
-INPUT_IMG_SIZE = (100, 100)
+LABELS = [
+    0, # Daisy
+    1, # Dandelion
+    2, # Rose
+    3, # Sun
+    4, # Tulip
+]
+
+INPUT_SIZE = (50, 50)
 
 ###############################################################################
 
@@ -125,41 +139,6 @@ def partition_sets(
         if src != dst:
             shutil.copyfile(src, dst)
 
-def resize_images(src_path, new_size):
-    if not isinstance(src_path, str):
-        raise Exception("src_path must be of type string.")
-    if isinstance(new_size, tuple):
-        if len(new_size) != 2:
-            raise Exception("Given size is not a valid size.")
-    else:
-        raise Exception("Given size must be a tuple.")
-
-    fnames = glob.glob("{}/*.jpg".format(src_path))
-    for fname in fnames:
-        file = os.path.join(src_path, fname)
-        src_img = cv2.imread(file)
-        if src_img.shape != new_size:
-            new_img = np.copy(src_img)
-            new_img = cv2.resize(new_img, new_size)
-            os.chdir(src_path)
-            cv2.imwrite(fname, new_img)
-            os.chdir(BASE_PATH)
-
-def add_imgs_labels(src_path, src_imgs, src_labels, label):
-    if not isinstance(src_path, str):
-        raise Exception("src_path must be a list.")
-    if not isinstance(src_imgs, list):
-        raise Exception("src_imgs must be a list.")
-    if not isinstance(src_labels, list):
-        raise Exception("src_labels must be a list.")
-
-    fnames = glob.glob("{}/*.jpg".format(src_path))
-    for fname in fnames:
-        img = cv2.imread(fname)
-        src_imgs.append(img)
-        src_labels.append(label)
-    return src_imgs, src_labels
-
 ###############################################################################
 
 # Checking that data path was created correctly 
@@ -176,133 +155,85 @@ path_checks(DATA_TULIP_PATH)
 ###############################################################################
 
 # Create folders for partitioned data
-Path(TRAIN_DAISY_PATH).mkdir(parents=True, exist_ok=True)
-Path(TRAIN_DANDE_PATH).mkdir(parents=True, exist_ok=True)
-Path(TRAIN_ROSE_PATH).mkdir(parents=True, exist_ok=True)
-Path(TRAIN_SUN_PATH).mkdir(parents=True, exist_ok=True)
-Path(TRAIN_TULIP_PATH).mkdir(parents=True, exist_ok=True)
-Path(VAL_DAISY_PATH).mkdir(parents=True, exist_ok=True)
-Path(VAL_DANDE_PATH).mkdir(parents=True, exist_ok=True)
-Path(VAL_ROSE_PATH).mkdir(parents=True, exist_ok=True)
-Path(VAL_SUN_PATH).mkdir(parents=True, exist_ok=True)
-Path(VAL_TULIP_PATH).mkdir(parents=True, exist_ok=True)
-Path(TEST_DAISY_PATH).mkdir(parents=True, exist_ok=True)
-Path(TEST_DANDE_PATH).mkdir(parents=True, exist_ok=True)
-Path(TEST_ROSE_PATH).mkdir(parents=True, exist_ok=True)
-Path(TEST_SUN_PATH).mkdir(parents=True, exist_ok=True)
-Path(TEST_TULIP_PATH).mkdir(parents=True, exist_ok=True)
+for pth in TRAIN_PATHS:
+    Path(pth).mkdir(parents=True, exist_ok=True)
+for pth in VAL_PATHS:
+    Path(pth).mkdir(parents=True, exist_ok=True)
+for pth in TEST_PATHS:
+    Path(pth).mkdir(parents=True, exist_ok=True)
 
 ###############################################################################
 
 print("Partitioning dataset, this could take a while...")
-partition_sets(DATA_DAISY_PATH, VAL_DAISY_PATH, 
-               TEST_DAISY_PATH, TRAIN_DAISY_PATH, "daisy")
-partition_sets(DATA_DANDE_PATH, VAL_DANDE_PATH,
-               TEST_DANDE_PATH, TRAIN_DANDE_PATH, "dandelion")
-partition_sets(DATA_ROSE_PATH, VAL_ROSE_PATH,
-               TEST_ROSE_PATH, TRAIN_ROSE_PATH, "rose")
-partition_sets(DATA_SUN_PATH, VAL_SUN_PATH,
-               TEST_SUN_PATH, TRAIN_SUN_PATH, "sunflower")
-partition_sets(DATA_TULIP_PATH, VAL_TULIP_PATH,
-               TEST_TULIP_PATH, TRAIN_TULIP_PATH, "tulip")
+for i, pth in enumerate(DATA_PATHS):
+    partition_sets(pth, VAL_PATHS[i], TEST_PATHS[i], TRAIN_PATHS[i], CLASSES[i])
 print("Partitioning completed.")
 
 ###############################################################################
 
-print("Resizing images, this could take a while...")
-resize_images(VAL_DAISY_PATH, INPUT_IMG_SIZE)
-resize_images(VAL_DANDE_PATH, INPUT_IMG_SIZE)
-resize_images(VAL_ROSE_PATH, INPUT_IMG_SIZE)
-resize_images(VAL_SUN_PATH, INPUT_IMG_SIZE)
-resize_images(VAL_TULIP_PATH, INPUT_IMG_SIZE)
-resize_images(TEST_DAISY_PATH, INPUT_IMG_SIZE)
-resize_images(TEST_DANDE_PATH, INPUT_IMG_SIZE)
-resize_images(TEST_ROSE_PATH, INPUT_IMG_SIZE)
-resize_images(TEST_SUN_PATH, INPUT_IMG_SIZE)
-resize_images(TEST_TULIP_PATH, INPUT_IMG_SIZE)
-resize_images(TRAIN_DAISY_PATH, INPUT_IMG_SIZE)
-resize_images(TRAIN_DANDE_PATH, INPUT_IMG_SIZE)
-resize_images(TRAIN_ROSE_PATH, INPUT_IMG_SIZE)
-resize_images(TRAIN_SUN_PATH, INPUT_IMG_SIZE)
-resize_images(TRAIN_TULIP_PATH, INPUT_IMG_SIZE)
-print("Resizing completed.")
+# import VGG model as base and add classification layers
+conv_base = applications.VGG16(weights='imagenet',
+                               include_top=False,
+                               input_shape=(INPUT_SIZE[0],INPUT_SIZE[1],3))
 
-###############################################################################
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(80, activation='relu'))
+model.add(layers.Dense(1, activation='softmax'))
 
-# prepare training image set and labels
-train_images = []
-train_labels = []
+# lock VGG model base as untrainable
+conv_base.trainable = False
 
-for i, path in enumerate(TRAIN_PATHS):
-    train_images, train_labels = add_imgs_labels(path, train_images,
-                                                 train_labels, LABELS[i])
-
-train_images = np.array(train_images)
-train_labels = np.array(train_labels)
-
-# prepare validation image set and labels
-val_images = []
-val_labels = []
-
-for i, path in enumerate(VAL_PATHS):
-    val_images, val_labels = add_imgs_labels(path, val_images,
-                                             val_labels, LABELS[i])
-
-val_images = np.array(val_images)
-val_labels = np.array(val_labels)
-
-# prepare test image set and labels
-test_images = []
-test_labels = []
-
-for i, path in enumerate(TEST_PATHS):
-    test_images, test_labels = add_imgs_labels(path, test_images,
-                                               test_labels, LABELS[i])
-
-test_images = np.array(test_images)
-test_labels = np.array(test_labels)
-
+# data augmentation generators and normalization
 train_datagen = image.ImageDataGenerator(
+    rescale=1.0/255,
     rotation_range=40,
     width_shift_range=0.2,
     height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
     horizontal_flip=True,
     vertical_flip=True,
+    fill_mode="nearest"
 )
-val_datagen = image.ImageDataGenerator()
-test_datagen = image.ImageDataGenerator()
+val_datagen = image.ImageDataGenerator(rescale=1.0/255)
+test_datagen = image.ImageDataGenerator(rescale=1.0/255)
 
-# normalize
-train_images = train_images.astype('float32') / 255
-val_images = val_images.astype('float32') / 255
-test_images = test_images.astype('float32') / 255
+train_gen = train_datagen.flow_from_directory(
+    TRAIN_PATH,
+    target_size=INPUT_SIZE,
+    batch_size=32,
+    class_mode="binary"
+)
+val_gen = val_datagen.flow_from_directory(
+    VAL_PATH,
+    target_size=INPUT_SIZE,
+    batch_size=32,
+    class_mode="binary"
+)
+test_gen = test_datagen.flow_from_directory(
+    TEST_PATH,
+    target_size=INPUT_SIZE,
+    batch_size=500, # all images in test_gen
+    class_mode="binary"
+)
 
-train_gen = train_datagen.flow((train_images, train_labels), batch_size=32)
-val_gen = val_datagen.flow((val_images, val_labels), batch_size=500)
-test_gen = test_datagen.flow((test_images, test_labels), batch_size=500)
+model.compile(
+    loss="binary_crossentropy",
+    optimizer=optimizers.RMSprop(lr=2e-5),
+    metrics=['acc']
+)
 
-# create network architecture
-network = models.Sequential()
-
-network.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=train_images[0].shape))
-network.add(layers.MaxPooling2D((2, 2)))
-network.add(layers.Conv2D(64, (3, 3), activation='relu'))
-network.add(layers.MaxPooling2D((2, 2)))
-network.add(layers.Conv2D(64, (3, 3), activation='relu'))
-network.add(layers.MaxPooling2D((2, 2)))
-network.add(layers.Conv2D(128, (3, 3), activation='relu'))
-network.add(layers.Flatten())
-network.add(layers.Dropout(0.5))
-network.add(layers.Dense(128, activation='relu'))
-network.add(layers.Dense(5, activation='softmax'))
-
-network.compile(optimizer='rmsprop',
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
-
-# fit network
-history=network.fit_generator(train_gen, steps_per_epoch=100, epochs=50,
-                              validation_data=val_gen, validation_steps=50)
+# train model
+history = model.fit_generator(
+    train_gen,
+    steps_per_epoch=100,
+    epochs=10,
+    validation_data=val_gen,
+    validation_steps=50,
+    verbose=1
+)
 
 # plotting network statistics
 acc = history.history['acc']
